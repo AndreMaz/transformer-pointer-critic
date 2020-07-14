d5be46432825551fc065a03789d06fe1a34f0feb
AndreMaz <andre.maz.90@gmail.com>
2020-07-14 15:21:35 +0100

    Cleaning

--- agents/agent_factory.py
+++ agents/agent_factory.py
@@ -1,18 +1,18 @@
+# Common stuff
+from agents.common.tester import test
+from agents.common.plotter import plotter
+
 # Double Pointer Critic
 from agents.double_pointer_critic.agent import DoublePointerCritic as DoublePointerCritic
 from agents.double_pointer_critic.trainer import trainer as DoublePointerCriticTrainer
-from agents.double_pointer_critic.tester import test as DoublePointerCriticTester
-from agents.double_pointer_critic.plotter import plotter as DoublePointerCriticPlotter
 
 # Double Transfomer Pointer Critic
 from agents.transformer_pointer_critic.agent import TransfomerPointerCritic
 from agents.transformer_pointer_critic.trainer import trainer as TransfomerPointerCriticTrainer
-from agents.transformer_pointer_critic.tester import test as TransfomerPointerCriticTester
-from agents.transformer_pointer_critic.plotter import plotter as TransfomerPointerCriticPlotter
 
 agents = {
-    'dpc': (DoublePointerCritic, DoublePointerCriticTrainer, DoublePointerCriticTester, DoublePointerCriticPlotter),
-    'tpc': (TransfomerPointerCritic, TransfomerPointerCriticTrainer, TransfomerPointerCriticTester, TransfomerPointerCriticPlotter),
+    'dpc': (DoublePointerCritic, DoublePointerCriticTrainer, test, plotter),
+    'tpc': (TransfomerPointerCritic, TransfomerPointerCriticTrainer, test, plotter),
 }
 
 
