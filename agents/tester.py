12d411db15e7ee881d00d9445f789c3beaab6877
AndreMaz <andre.maz.90@gmail.com>
2020-08-18 15:08:34 +0100

    Fixed the or tool bug

--- agents/tester.py
+++ agents/tester.py
@@ -5,13 +5,6 @@ from agents.optimum_solver import solver
 import numpy as np
 
 def test(env: KnapsackV2, agent: Agent):
-    # Compute optimal values
-    optimal_values = []
-    for index in range(env.batch_size):
-        data = env.convert_to_ortools_input(index)
-        optimal_values.append(solver(data, False))
-
-    # Test the net
     # Set the agent to testing mode
     agent.training = False
     agent.stochastic_action_selection = False
@@ -22,6 +15,13 @@ def test(env: KnapsackV2, agent: Agent):
     episode_rewards = np.zeros((agent.batch_size, agent.num_items), dtype="float32")
     current_state, backpack_net_mask, item_net_mask, mha_used_mask = env.reset()
     dec_input = agent.generate_decoder_input(current_state)
+
+    # Compute optimal values
+    optimal_values = []
+    for index in range(env.batch_size):
+        data = env.convert_to_ortools_input(index)
+        optimal_values.append(solver(data, False))
+
 
     while not isDone:
         # Select an action
