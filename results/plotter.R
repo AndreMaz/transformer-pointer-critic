3d978297b577536457ab59635d476bf059a5b133
AndreMaz <andre.maz.90@gmail.com>
2020-12-02 11:08:24 +0000

    Fix ggplot

--- results/plotter.R
+++ results/plotter.R
@@ -10,11 +10,15 @@ net_og_data <- read.csv(file='./resource/test_net.csv', header = TRUE, sep = ';'
 heuristic_og_data <- read.csv(file='./resource/test_heu.csv', header = TRUE, sep = ';')
 
 # Stack/Merge the stats
-og_data <- merge(net_og_data, heuristic_og_data)
+og_data <- rbind(net_og_data, heuristic_og_data)
+
+# og_data <- og_data %>%
+#  mutate(Node = paste('Node',Node))
 
 # Set up factor variables
 og_data$Node <- as.factor(og_data$Node)
 og_data$Step <- as.factor(og_data$Step)
+og_data$Method <- as.factor(og_data$Method)
 
 branded_colors <- list(
   "blue"   = "#00798c",
@@ -26,16 +30,21 @@ branded_colors <- list(
 )
 
 # Reshape into tall format
-reshaped <- melt(og_data, id.vars = c("Step","Node"), variable.name = 'Type', value.name = 'Value')
+reshaped <- melt(og_data, id.vars = c("Method", "Step","Node", "Percentage_Penalized"), variable.name = 'Type', value.name = 'Value')
 
 reshaped <- reshaped %>%
+  # filter(Node != "Node 0")
   filter(Node != 0)
 
+facet_labeller <- function(variable,value){
+  return (value)
+}
+
 ggplot(data = reshaped)+
-  geom_point(aes(x=Step, y=Value, col=Type), alpha=0.3)+
+  geom_point(aes(x=Step, y=Value, col=Type, group=Method), alpha=0.3)+
   # geom_line(aes(x=Step, y=Value, col=Type, group=Type), linetype='dashed')+
   # geom_line(aes(x=Step, y=Value, col=Type, group=Type), linetype='dotted')+
-  facet_wrap(~Node)+
+  facet_wrap(Node~Method, labeller = labeller(Node=label_both, Method=label_both) ,ncol = 4)+
   labs(x="Placement Step", y='Load (%)')+
   scale_x_discrete(breaks = seq(10, 600, by = 20))+
   theme(
