456a912173bc8791d4e89fee386e5173dc3f8ea3
AndreMaz <andre.maz.90@gmail.com>
2021-02-13 12:11:21 +0000

    Update the ggplot script

--- results/learning.R
+++ results/learning.R
@@ -8,8 +8,42 @@ library(nortest)
 ## Load data from CSV
 learning_data <- read.csv(file='../media/plots/ResourceV3/transformer/training.csv', header = TRUE, sep = ';')
 
+# Reshape learning stats into tall format
+learning_stats <- melt(learning_data, id.vars = c(
+  "Step",
+  "Avg.Reward",
+  "Min.Reward",
+  "Max.Reward"
+), variable.name = 'Type', value.name = 'Value')
 
-# Reshape into tall format
+## Filter out by Types of Sets
+learning_stats <- learning_stats %>%
+  filter(
+    Type != 'Value.Loss' &
+      Type != 'Total.Bin.Loss' &
+      Type != 'Bin.Policy.Loss' # &
+    # Type != 'Bin.Entropy' &
+    # Type != 'Total.Resource.Loss' &
+    # Type != 'Resource.Policy.Loss' &
+    # Type != 'Resource.Entropy'
+  )
+# filter( Step > 5000)
+
+# Plot Losses and entropy
+ggplot(data = learning_stats, aes(x=Step, y=Value, col=Type, group = Type))+
+  geom_point(alpha=0.3)+
+  #geom_line(alpha=1.0)+
+  # geom_smooth(alpha=0.3, span=0.3, se = FALSE)+
+  # geom_line(aes(x=Step, y=Value, col=Type, group=Type), size=1.5, alpha=0.7)+
+  labs(x="Episode", y='Value')+
+  # scale_x_discrete()+
+  theme(
+    # legend.position="bottom",
+    # legend.title=element_blank()
+  )
+
+
+# Reshape reward data into tall format
 reward_stats <- melt(learning_data, id.vars = c(
   "Step",
   "Value.Loss",
@@ -31,31 +65,4 @@ ggplot(data = reward_stats, aes(x=Step, y=Value, col=Type, group = Type))+
     # legend.position="bottom",
     # legend.title=element_blank()
   )
-
-
-learning_stats <- melt(learning_data, id.vars = c(
-  "Step",
-  "Avg.Reward",
-  "Min.Reward",
-  "Max.Reward"
-  ), variable.name = 'Type', value.name = 'Value')
-
-## Filter out by Types of Sets
-learning_stats <- learning_stats %>%
- filter( Type != 'Value.Loss' & Type != 'Total.Bin.Loss' & Type != 'Total.Resource.Loss')
-# filter( Step > 5000)
-
-# Plot Losses and entropy
-ggplot(data = learning_stats, aes(x=Step, y=Value, col=Type, group = Type))+
-  geom_point(alpha=0.3)+
-  #geom_line(alpha=1.0)+
-  # geom_smooth(alpha=0.3, span=0.3, se = FALSE)+
-  # geom_line(aes(x=Step, y=Value, col=Type, group=Type), size=1.5, alpha=0.7)+
-  labs(x="Episode", y='Value')+
-  # scale_x_discrete()+
-  theme(
-    # legend.position="bottom",
-    # legend.title=element_blank()
-  )
-
 
