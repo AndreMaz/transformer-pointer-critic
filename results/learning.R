e1c1116302913893267575fc74aba03f18efddd5
AndreMaz <andre.maz.90@gmail.com>
2021-02-17 13:25:26 +0000

    Tweak ggplot plot fn

--- results/learning.R
+++ results/learning.R
@@ -19,13 +19,13 @@ learning_stats <- melt(learning_data, id.vars = c(
 ## Filter out by Types of Sets
 learning_stats <- learning_stats %>%
   filter(
-    Type != 'Value.Loss' &
-    Type != 'Total.Bin.Loss' &
-    Type != 'Total.Resource.Loss' &
-    Type != 'Bin.Policy.Loss'  &
-    Type != 'Bin.Entropy' #&
-    # Type != 'Resource.Policy.Loss' &
-    # Type != 'Resource.Entropy'
+    Type == 'Value.Loss' #|
+    # Type == 'Total.Bin.Loss' #|
+    # Type == 'Total.Resource.Loss' |
+    # Type == 'Bin.Policy.Loss'  |
+    # Type == 'Bin.Entropy' |
+    # Type == 'Resource.Policy.Loss' |
+    # Type == 'Resource.Entropy'
   )
 # filter( Step > 5000)
 
